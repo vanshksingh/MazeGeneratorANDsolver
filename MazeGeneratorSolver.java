5313c10bcee2e7cfe4c93b6991af06ff002f75fa
Vansh Kumar Singh <114809624+vanshksingh@users.noreply.github.com>
2023-11-07 02:44:25 +0530

    Update MazeGeneratorSolver.java

--- MazeGeneratorSolver.java
+++ MazeGeneratorSolver.java
@@ -15,91 +15,44 @@ public class MazeGeneratorSolver {
     }
 
     public void generateMaze() {
-        // Initialize the maze with walls
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 maze[i][j] = WALL;
             }
         }
 
-        Random random = new Random();
-        int startX = random.nextInt(cols);
-        int startY = random.nextInt(rows);
-
-        generateMazeRecursive(startX, startY);
+        recursiveDivision(1, 1, cols - 2, rows - 2);
     }
 
-    private void generateMazeRecursive(int x, int y) {
-        maze[y][x] = PATH;
-
-        int[] directions = { 1, 2, 3, 4 };
-        Collections.shuffle(Arrays.asList(directions));
-
-        for (int direction : directions) {
-            int nextX = x;
-            int nextY = y;
-
-            switch (direction) {
-                case 1: // Up
-                    nextY -= 2;
-                    break;
-                case 2: // Down
-                    nextY += 2;
-                    break;
-                case 3: // Left
-                    nextX -= 2;
-                    break;
-                case 4: // Right
-                    nextX += 2;
-                    break;
-            }
-
-            if (nextX > 0 && nextX < cols - 1 && nextY > 0 && nextY < rows - 1 && maze[nextY][nextX] == WALL) {
-                maze[nextY][nextX] = PATH;
-                maze[y + (nextY - y) / 2][x + (nextX - x) / 2] = PATH;
-                generateMazeRecursive(nextX, nextY);
-            }
+    private void recursiveDivision(int x, int y, int width, int height) {
+        if (width < 2 || height < 2) {
+            return;
         }
+
+        int wallX = x + 2 + (int) (Math.random() * (width - 2) / 2) * 2;
+        int wallY = y + 2 + (int) (Math.random() * (height - 2) / 2) * 2;
+
+        for (int i = y; i <= y + height; i++) {
+            maze[i][wallX] = WALL;
+        }
+
+        for (int j = x; j <= x + width; j++) {
+            maze[wallY][j] = WALL;
+        }
+
+        int gapX = x + 2 + (int) (Math.random() * (width - 2) / 2) * 2;
+        int gapY = y + 2 + (int) (Math.random() * (height - 2) / 2) * 2;
+
+        maze[gapY][gapX] = PATH;
+
+        recursiveDivision(x, y, wallX - x - 1, wallY - y - 1);
+        recursiveDivision(wallX + 1, y, x + width - wallX - 1, wallY - y - 1);
+        recursiveDivision(x, wallY + 1, wallX - x - 1, y + height - wallY - 1);
+        recursiveDivision(wallX + 1, wallY + 1, x + width - wallX - 1, y + height - wallY - 1);
     }
 
     public void solveMaze() {
-        boolean[][] visited = new boolean[rows][cols];
-        Stack<Point> stack = new Stack<>();
-        Point start = new Point(1, 1);
-        Point end = new Point(cols - 2, rows - 2);
-
-        stack.push(start);
-
-        while (!stack.isEmpty()) {
-            Point current = stack.peek();
-            int x = current.x;
-            int y = current.y;
-            visited[y][x] = true;
-
-            if (current.equals(end)) {
-                return; // Maze solved
-            }
-
-            int[] dx = {1, 0, -1, 0};
-            int[] dy = {0, 1, 0, -1};
-
-            boolean found = false;
-
-            for (int i = 0; i < 4; i++) {
-                int nextX = x + dx[i];
-                int nextY = y + dy[i];
-
-                if (nextX >= 0 && nextX < cols && nextY >= 0 && nextY < rows && !visited[nextY][nextX] && maze[nextY][nextX] == PATH) {
-                    stack.push(new Point(nextX, nextY));
-                    found = true;
-                    break;
-                }
-            }
-
-            if (!found) {
-                stack.pop(); // Backtrack
-            }
-        }
+        
     }
 
     public void printMaze() {
@@ -116,18 +69,20 @@ public class MazeGeneratorSolver {
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         MazeGeneratorSolver mazeGenerator = new MazeGeneratorSolver(21, 21);
 
-        while (true) {
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
+
+        while (!exit) {
             System.out.println("Menu:");
             System.out.println("1. Generate Maze");
             System.out.println("2. Solve Maze");
             System.out.println("3. Exit");
-
             System.out.print("Enter your choice: ");
+            
             int choice = scanner.nextInt();
-
+            
             switch (choice) {
                 case 1:
                     mazeGenerator.generateMaze();
@@ -139,35 +94,13 @@ public class MazeGeneratorSolver {
                     break;
                 case 3:
                     System.out.println("Exiting program.");
-                    scanner.close();
-                    System.exit(0);
+                    exit = true;
                     break;
                 default:
                     System.out.println("Invalid choice. Please select a valid option.");
                     break;
             }
         }
-    }
-}
-
-class Point {
-    int x, y;
-
-    Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        Point point = (Point) obj;
-        return x == point.x && y == point.y;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(x, y);
+        scanner.close();
     }
 }
